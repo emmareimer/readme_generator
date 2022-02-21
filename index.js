2f25704d9c79dfc55abd1d801c50da4e1e990249
emma@beinproximity.com <emma@beinproximity.com>
2022-02-21 08:58:54 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+"use strict";
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the name of your application?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Write a brief description of your application.",
+  },
+  {
+    type: "input",
+    name: "uses",
+    message: "What are the uses for your application?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the steps for installing your application?",
+  },
+
+  {
+    type: "input",
+    name: "issues",
+    message: "How can you report issues for your application?",
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "How can others contribute to your application?",
+  },
+  {
+    type: "input",
+    name: "resources",
+    message: "Do you need to cite any resources?",
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  let data = JSON.stringify(answers, null, "  ");
+
+  fs.appendFile("log.txt", data, (err) =>
+    // if error, console log error, else console log commit logged
+    err ? console.error(err) : console.log("Commit logged!")
+  );
+});
